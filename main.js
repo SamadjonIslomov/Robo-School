ec51c3861a6f2eeeeb3861c479717e8df6ad5933
SamadjonIslomov <149060290+SamadjonIslomov@users.noreply.github.com>
2024-02-19 21:03:44 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,37 +1,52 @@
 const btn1 = document.querySelector(".btn_1");
 const btn2 = document.querySelectorAll(".btn-paket");
 const modal1 = document.querySelector(".modal");
-const modal1Form = document.querySelector(".zapisat-kurs-form");
 const modal2 = document.querySelector(".modal-price");
 const overlay = document.querySelector(".overlay");
-const zayavkaBtn = document.querySelector(".input-bottom.color");
 
 function toggleModal(modal, isOpen) {
   const action = isOpen ? "add" : "remove";
   modal.classList[action]("open");
   overlay.classList[action]("open");
 }
+
+function handleFormSubmit(e, modal) {
+  e.preventDefault();
+  const name = document.querySelector("#name").value;
+  const lastName = document.querySelector("#LastName").value;
+  const telNumber = document.querySelector("#telNumber").value;
+  console.log(
+    "Ism:",
+    name,
+    "Familiya:",
+    lastName,
+    "Telefon raqami:",
+    telNumber
+  );
+  toggleModal(modal, false);
+}
+
 btn1.addEventListener("click", () => toggleModal(modal1, true));
 btn2.forEach((btn) =>
   btn.addEventListener("click", () => toggleModal(modal2, true))
 );
-modal1Form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  toggleModal(modal1, false);
+
+[modal1, modal2].forEach((modal) => {
+  modal.addEventListener("submit", (e) => handleFormSubmit(e, modal));
 });
+
 window.addEventListener("keydown", (event) => {
   if (event.key === "Escape") {
-    toggleModal(modal1, false);
-    toggleModal(modal2, false);
+    [modal1, modal2].forEach((modal) => toggleModal(modal, false));
   }
 });
+
 overlay.addEventListener("click", () => {
-  toggleModal(modal1, false);
-  toggleModal(modal2, false);
+  [modal1, modal2].forEach((modal) => toggleModal(modal, false));
 });
-zayavkaBtn.addEventListener("click", () => {
-  const inputs = document.querySelectorAll(".input-bottom");
-  inputs.forEach((input) => {
+
+document.querySelector(".input-bottom.color").addEventListener("click", () => {
+  document.querySelectorAll(".input").forEach((input) => {
     input.value = "";
   });
 });
